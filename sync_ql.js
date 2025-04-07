f1c328eb1b558a72c41b62785f6e1d3b46482ec0
xiaomiejiang99 <76171920+xiaomiejiang99@users.noreply.github.com>
2025-04-07 18:51:25 +0800

    Update sync_ql.js

--- sync_ql.js
+++ sync_ql.js
@@ -50,12 +50,6 @@ async function getToken() {
             $.log(`📢 新增变量: ${env_key}没有初始化，请先完成抓取或者设置后，再同步`)
             content += `新增变量: ${env_key}没有初始化，请先完成抓取或者设置后，再同步 \n`
         }
-        if(active_code_value) {
-            await search(active_code_key, active_code_value);
-        }else{
-            $.log(`📢 新增变量: ${active_code_key}没有初始化，请先完成抓取或者设置后，再同步`)
-            content += `新增变量: ${active_code_key}没有初始化，请先完成抓取或者设置后，再同步 \n`
-        }
     } else {
         $.log(`获取Token失败: ${result.message}`)
         content += `获取Token失败: ${result.message} \n`
