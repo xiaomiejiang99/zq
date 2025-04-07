edd4243118cf03620ea6b6fffdd1ee640b1c5ec8
xiaomiejiang99 <76171920+xiaomiejiang99@users.noreply.github.com>
2025-04-07 21:20:00 +0800

    Update sync_ql2.js

--- sync_ql2.js
+++ sync_ql2.js
@@ -8,7 +8,7 @@ let env_remark = $.getdata('sync_ql_remark')
 // 0:为关闭日志，1:为开启
 const debug = 0; 
 // 0:关闭通知，1:默认开启
-//let tz = ($.getval('tz') || '1');
+let tz = ($.getval('tz') || '1');
 let httpResult = ''
 let content = ''
 let token = ''
